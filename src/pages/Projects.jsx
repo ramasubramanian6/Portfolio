4172869674cf3d17990b686df163727ab9f8896c
ramasubramanian6 <ramsubramanianponni37@gmail.com>
2024-08-02 22:15:55 +0530

    color changes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,8 +5,8 @@ import rf from '../assets/sRF.jpg';
 
 function Projects() {
   return (
-    <div className='pl-5 pt-12 bottom-0  bg-gradient-to-r white from-pink-500  to-white ' id='project'>
-        <div className=' text-white text-2xl w-24 font-semibold  md:text-4xl md:text-center md:w-52 border-b-4 border-yellow-500 font-logodis'>Projects</div>
+    <div className='pl-5 pt-12 bottom-0  bg-gradient-to-r white from-yellow-500  to-white ' id='project'>
+        <div className=' text-black text-2xl w-24 font-semibold  md:text-4xl md:text-center md:w-52 border-b-4 border-yellow-50 border-opacity-95 font-logodis'>Projects</div>
         <div className='flex flex-col md:flex-row justify-evenly pl-16 md:pl-[4rem] pt-11 text-white  '>
             <div className='md:top-0 md:w-[18rem] md:pb-16 flex flex-col  md:flex-col pl-2 relative group' >
                 <img src={rf} alt="Movie database"className='size-[13rem] md:size-[17rem] object-cover object-left rounded-2xl ' ></img>
